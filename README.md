7f404511c2130d44e370a57e043388bc9a001a94
jcuellar@prointegra.com <joa.cuellar@riseup.net>
2015-10-07 13:21:00 +0200

    uploaded script

---
+++ README.md
@@ -0,0 +1,13 @@
+LICENSE HEADER SCRIPT
+===========
+
+##DESCRIPTION
+----------
+It's a simply bash script to automatically modify your source code files license header
+It's cat a license file included to the top of your .cpp .h (.c .py ...) files
+
+If you have forked an app it removes x lines of every file and add yours license header text file.
+
+##USAGE
+------------
+> sh header_Script.sh License_header
---
+++ header_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+ #
+ # license header script v1
+ #
+ # Copyright (C) 2015 Joaquín Cuéllar
+ #
+ # license header script is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # license header script is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with the program.  If not, see <http://www.gnu.org/licenses/>.
+ #
+ #
+
+for i in *.h *.cpp #or *.h # or whatever other pattern...
+do
+  if ! grep -q Copyright $i
+  then
+    cat License_header $i >$i.new && mv $i.new $i
+  else
+    count=`head -25 "$i" |wc -c`
+    dd if="$i" bs="$count" skip=1 of="$i" conv=notrunc
+    truncate -s "-$count" "$i"
+  fi
+done
